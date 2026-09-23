4f6e12d526d8d75d532bcc7e2cafb5a0cb3128fd
Prakash <kupraki@amazon.com>
2026-09-23 21:20:17 +0530

    chore: set default versionName to 0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = (project.findProperty("appVersionCode") as? String)?.toInt() ?: 1
-        versionName = (project.findProperty("appVersionName") as? String) ?: "0.0.2"
+        versionName = (project.findProperty("appVersionName") as? String) ?: "0.0.3"
 
         testInstrumentationRunner = "com.itsluminous.cleartravel.HiltTestRunner"
 
